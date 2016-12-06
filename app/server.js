9c8656f7afe211e324f5e43ea3193497f78d0121
Kai Hsia <kaihsia@gmail.com>
2016-12-06 10:49:47 -0800

    recovered stash

--- app/server.js
+++ app/server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + '/../dist')));
 
+// GET ALL TWEETS MANUALLY
 // app.get('/tweets', function(req, res) {
 //   twitter('press', function(arr) {
 //     res.send(arr);
